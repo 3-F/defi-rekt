a76b5d023e1dafb5b49ae2a34484ad06c8348947
3-F <junjie_fei@zju.edu.cn>
2022-01-19 19:17:53 +0800

    fix: hardhat template test/index.ts

--- misc/hardhat-template/test/index.ts
+++ misc/hardhat-template/test/index.ts
@@ -6,10 +6,10 @@ import { ethers } from "hardhat";
 describe("Greeter", function () {
   it("desc ...", async function () {
     const accounts = await ethers.getSigners()
-    const hacker = await accounts[0].getAddress();
+    const hacker = await accounts[0];
 
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy(hacker);
+    const Greeter = await ethers.getContractFactory("Greeter", hacker);
+    const greeter = await Greeter.deploy();
 
     await greeter.deployed();
     await greeter.greet("Happy Hack");
